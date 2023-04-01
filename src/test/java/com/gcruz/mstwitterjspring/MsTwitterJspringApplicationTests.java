ecba0dfd2070031a609eb58375101e6cca95f9a3
Gabriel Cruz <gabrielcruzsoto@gmail.com>
2023-03-31 23:34:14 -0300

    first commit

---
+++ src/test/java/com/gcruz/mstwitterjspring/MsTwitterJspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcruz.mstwitterjspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTwitterJspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
